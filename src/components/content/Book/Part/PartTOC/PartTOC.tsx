e5af5db8f12c6e65c6bd2aae5e20d7a07c8f3066
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-09-25 19:51:53 -0400

    ADD - Book Table of Contents components

---
+++ src/components/content/Book/Part/PartTOC/PartTOC.tsx
@@ -0,0 +1,36 @@
+import type { FC } from "react";
+import Link from "next/link";
+
+import type Part from "@/models/Book/Part";
+import ChapterTOC from "@/components/content/Book/Chapter/ChapterTOC/ChapterTOC";
+
+/**
+ * Part Table of Contents
+ */
+const PartTOC: FC<{ baseURL: string; part: Part; showTitle?: boolean }> = ({
+	baseURL,
+	part,
+	showTitle = true,
+}) => {
+	/*
+	 * React element
+	 */
+	return (
+		<div>
+			{showTitle && (
+				<h2>
+					<Link href={`${baseURL}/${part.slug}`}>{part.title}</Link>
+				</h2>
+			)}
+			<ol className="no-style">
+				{part.chapters.map((chapter) => (
+					<li key={chapter.slug}>
+						<ChapterTOC baseURL={`${baseURL}/${part.slug}`} chapter={chapter} />
+					</li>
+				))}
+			</ol>
+		</div>
+	);
+};
+
+export default PartTOC;

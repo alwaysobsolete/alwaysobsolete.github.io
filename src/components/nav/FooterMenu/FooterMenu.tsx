1d839ae97db861d5273df83b3bc186822b81f364
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-09-30 01:19:38 -0400

    UPDATE - Add section nav links to FooterMenu component

--- src/components/nav/FooterMenu/FooterMenu.tsx
+++ src/components/nav/FooterMenu/FooterMenu.tsx
@@ -6,8 +6,18 @@ import styles from "./styles.module.scss";
 const FooterMenu: FC = () => {
 	return (
 		<nav className={styles.wrapper}>
-			<div className={styles.brand}>
-				<Link href="/">/always/obsolete</Link>
+			<div className={styles.branding}>
+				<span className={styles.logo}>
+					<Link href="/">/always/obsolete</Link>
+				</span>
+			</div>
+
+			<div className={styles.section}>
+				<ul>
+					<li>
+						<Link href="/books">Books</Link>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);

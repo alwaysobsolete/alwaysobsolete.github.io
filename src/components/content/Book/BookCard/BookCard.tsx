6cdbfb0e5c8d14ac4b75681bb7b09474b6af3590
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-01 02:21:11 -0400

    UPDATE - Wrap image in link in BookCard component

--- src/components/content/Book/BookCard/BookCard.tsx
+++ src/components/content/Book/BookCard/BookCard.tsx
@@ -12,15 +12,25 @@ import styles from "./styles.module.scss";
 const BookCard: FC<{
 	book: Book;
 }> = ({ book }) => {
+	/*
+	 * Constants
+	 */
+	const url = `/books/${book.slug}`;
+
+	/*
+	 * React element
+	 */
 	return (
 		<Card className={styles.wrapper}>
 			<Card.Section className={styles.section}>
 				{book.imgSrc && (
-					<Image alt={`Cover art for ${book.title}`} fill src={book.imgSrc} />
+					<Link href={url}>
+						<Image alt={`Cover art for ${book.title}`} fill src={book.imgSrc} />
+					</Link>
 				)}
 			</Card.Section>
 			<h2>
-				<Link href={`/books/${book.slug}`}>{book.title}</Link>
+				<Link href={url}>{book.title}</Link>
 			</h2>
 			<p>{book.description}</p>
 		</Card>

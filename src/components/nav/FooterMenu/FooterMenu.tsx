a6f2258bc0291e444f443977360c3cc3f3d6ce51
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-02 01:09:52 -0400

    UPDATE - Add GitHub icon to FooterMenu

--- src/components/nav/FooterMenu/FooterMenu.tsx
+++ src/components/nav/FooterMenu/FooterMenu.tsx
@@ -1,11 +1,23 @@
 import type { FC } from "react";
 import Link from "next/link";
 import { SocialIcon } from "react-social-icons/component";
+import "react-social-icons/github";
 import "react-social-icons/itch.io";
 
 import styles from "./styles.module.scss";
 
 const FooterMenu: FC = () => {
+	/*
+	 * Constants
+	 */
+	const socialIconStyle = {
+		height: "2rem",
+		width: "2rem",
+	};
+
+	/*
+	 * React element
+	 */
 	return (
 		<nav className={styles.wrapper}>
 			<div className={styles.branding}>
@@ -13,12 +25,21 @@ const FooterMenu: FC = () => {
 					<Link href="/">/always/obsolete</Link>
 				</div>
 
-				<div className={styles.icons}>
-					<SocialIcon
-						style={{ height: "2rem", width: "2rem" }}
-						url="https://alwaysobsolete.itch.io"
-					/>
-				</div>
+				<ul className={styles.icons}>
+					<li>
+						<SocialIcon
+							style={socialIconStyle}
+							url="https://alwaysobsolete.itch.io"
+						/>
+					</li>
+
+					<li>
+						<SocialIcon
+							style={socialIconStyle}
+							url="https://github.com/alwaysobsolete"
+						/>
+					</li>
+				</ul>
 			</div>
 
 			<div className={styles.section}>

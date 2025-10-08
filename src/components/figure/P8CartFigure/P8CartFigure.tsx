8e30317ab8cebf855a38bb9e98675db1ab2c23ae
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-08 13:32:16 -0400

    ADD - P8CartFigure component

---
+++ src/components/figure/P8CartFigure/P8CartFigure.tsx
@@ -0,0 +1,28 @@
+import type { FC } from "react";
+
+import type { ImageFigureProps } from "@/components/figure/ImageFigure/ImageFigure";
+import ImageFigure from "@/components/figure/ImageFigure/ImageFigure";
+
+const P8CartFigure: FC<ImageFigureProps> = ({
+	alt,
+	caption,
+	height,
+	href,
+	src,
+	title,
+	width,
+}) => {
+	return (
+		<ImageFigure
+			alt={alt}
+			caption={caption || alt}
+			height={height}
+			href={href || src}
+			src={src}
+			title={title || "This image is a p8.png cart, run it in Pico-8!"}
+			width={width}
+		/>
+	);
+};
+
+export default P8CartFigure;

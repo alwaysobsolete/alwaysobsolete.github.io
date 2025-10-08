ad94475199c1d042277540032f59809ba14edbb2
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-08 11:55:55 -0400

    UPDATE - Use Video component in mdx-components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,23 @@
 import type { MDXComponents } from "mdx/types";
 
-const components: MDXComponents = {};
+import Video from "@/components/video/Video/Video";
 
-export function useMDXComponents(): MDXComponents {
+// This file allows you to provide custom React components
+// to be used in MDX files. You can import and use any
+// React component you want, including inline styles,
+// components from other libraries, and more.
+
+const components: MDXComponents = {
+	Video,
+};
+
+/**
+ * Use MDX Components
+ *
+ * @see https://nextjs.org/docs/app/guides/mdx#global-styles-and-components
+ */
+function useMDXComponents(): MDXComponents {
 	return components;
 }
+
+export { useMDXComponents };

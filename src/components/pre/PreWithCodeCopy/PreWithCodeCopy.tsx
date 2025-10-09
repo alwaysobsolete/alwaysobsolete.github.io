06e18122b0ee4159b8f74096908967dad787d614
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-09 15:58:55 -0400

    UPDATE - Add Popover to PreWithCodeCopy component

--- src/components/pre/PreWithCodeCopy/PreWithCodeCopy.tsx
+++ src/components/pre/PreWithCodeCopy/PreWithCodeCopy.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { FC, PropsWithChildren } from "react";
-import { Button, CopyButton } from "@mantine/core";
+import { Button, CopyButton, Popover } from "@mantine/core";
 import Check from "@material-symbols/svg-400/outlined/check.svg";
 import ContentCopy from "@material-symbols/svg-400/outlined/content_copy.svg";
 import { isValidElement } from "react";
@@ -34,18 +34,25 @@ const PreWithCodeCopy: FC<PropsWithChildren> = ({ children }) => {
 		<pre className={styles.wrapper}>
 			<CopyButton value={children.props.children}>
 				{({ copied, copy }) => (
-					<Button
-						className={styles.copyBtn}
-						color={copied ? "teal" : "gray"}
-						onClick={copy}
-						title={copied ? "Copied" : "Copy to Clipboard"}
-					>
-						{copied ? (
-							<Check height="1rem" width="1rem" />
-						) : (
-							<ContentCopy height="1rem" width="1rem" />
-						)}
-					</Button>
+					<Popover opened={copied} position="bottom" shadow="md" withArrow>
+						<Popover.Target>
+							<Button
+								className={styles.copyBtn}
+								color={copied ? "teal" : "gray"}
+								onClick={copy}
+								title={copied ? "Copied" : "Copy to Clipboard"}
+							>
+								{copied ? (
+									<Check height="1rem" width="1rem" />
+								) : (
+									<ContentCopy height="1rem" width="1rem" />
+								)}
+							</Button>
+						</Popover.Target>
+						<Popover.Dropdown className={styles.popOver}>
+							Copied!
+						</Popover.Dropdown>
+					</Popover>
 				)}
 			</CopyButton>
 

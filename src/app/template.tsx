79d92698c8675a5e0f4ced563ba1c16b5167fdc7
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-11 17:42:36 -0400

    ADD - Root template

---
+++ src/app/template.tsx
@@ -0,0 +1,10 @@
+import type { FC, PropsWithChildren } from "react";
+
+/**
+ * Root Template
+ */
+const RootTemplate: FC<PropsWithChildren> = ({ children }) => {
+	return <>{children}</>;
+};
+
+export default RootTemplate;

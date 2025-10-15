fa24408a2c5f5c9f2f9a037e5e85beea10b835a5
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-14 23:56:32 -0400

    UPDATE - robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,10 +9,44 @@ export const dynamic = "force-static";
 
 function robots(): MetadataRoute.Robots {
 	return {
-		rules: {
-			userAgent: "ia_archiver",
-			disallow: "/",
-		},
+		rules: [
+			{
+				userAgent: "Bytespider",
+				disallow: "/",
+			},
+			{
+				userAgent: "ChatGPT-User",
+				disallow: "/",
+			},
+			{
+				userAgent: "ClaudeBot",
+				disallow: "/",
+			},
+			{
+				userAgent: "CCBot",
+				disallow: "/",
+			},
+			{
+				userAgent: "Google-Extended",
+				disallow: "/",
+			},
+			{
+				userAgent: "GPTBot",
+				disallow: "/",
+			},
+			{
+				userAgent: "ia_archiver",
+				disallow: "/",
+			},
+			{
+				userAgent: "OAI-SearchBot",
+				disallow: "/",
+			},
+			{
+				userAgent: "PerplexityBot",
+				disallow: "/",
+			},
+		],
 		sitemap: `${env.URL}/sitemap.xml`,
 	};
 }

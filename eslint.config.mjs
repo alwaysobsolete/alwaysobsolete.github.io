0fd1087a35d4ab41b88786aa1716941b431fb33a
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-10-19 01:27:17 -0400

    UPDATE - Ignore public directory in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,29 @@
 import { includeIgnoreFile } from "@eslint/compat";
+import { defineConfig, globalIgnores } from "eslint/config";
 import { FlatCompat } from "@eslint/eslintrc";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 // Use .gitignore
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, ".gitignore");
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 // Legacy compatibility
 const compat = new FlatCompat({
-	// import.meta.dirname is available after Node.js v20.11.0
 	baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = defineConfig([
 	...compat.config({
 		extends: ["next/core-web-vitals", "next/typescript"],
 	}),
 	includeIgnoreFile(gitignorePath),
+	globalIgnores(["public/**/*"]),
 	eslintPluginPrettierRecommended,
 	{
 		rules: {
 			semi: "error",
 		},
 	},
-];
+]);
 
 export default eslintConfig;

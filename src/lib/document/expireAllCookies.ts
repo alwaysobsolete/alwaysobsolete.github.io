f2308bbb88ce11a32f7f264087874d75c42bfd12
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-11-15 21:48:56 -0500

    UPDATE - Remove client check in expireCookie(), expireAllCookies()

--- src/lib/document/expireAllCookies.ts
+++ src/lib/document/expireAllCookies.ts
@@ -1,13 +1,13 @@
+"use client";
+
 import expireCookie from "./expireCookie";
 
 function expireAllCookies() {
-	if (typeof window !== "undefined" && document) {
-		const cookieNames = document.cookie
-			.split(";")
-			.map((cookie) => cookie.split("=")[0]);
+	const cookieNames = document.cookie
+		.split(";")
+		.map((cookie) => cookie.split("=")[0]);
 
-		cookieNames.forEach((cookieName) => expireCookie(cookieName));
-	}
+	cookieNames.forEach((cookieName) => expireCookie(cookieName));
 }
 
 export default expireAllCookies;
--- src/lib/document/expireCookie.ts
+++ src/lib/document/expireCookie.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import env from "@/config/env";
 
 function expireCookie(
@@ -5,12 +7,10 @@ function expireCookie(
 	domain = env.NEXT_PUBLIC_DOMAIN,
 	path = "/",
 ) {
-	if (typeof window !== undefined && document) {
-		const expires = "Thu, 01 Jan 1970 00:00:00 UTC";
+	const expires = "Thu, 01 Jan 1970 00:00:00 UTC";
 
-		document.cookie = `${name}=; domain=${domain}; expires=${expires}; path=${path};`;
-		document.cookie = `${name}=; domain=.${domain}; expires=${expires}; path=${path};`;
-	}
+	document.cookie = `${name}=; domain=${domain}; expires=${expires}; path=${path};`;
+	document.cookie = `${name}=; domain=.${domain}; expires=${expires}; path=${path};`;
 }
 
 export default expireCookie;

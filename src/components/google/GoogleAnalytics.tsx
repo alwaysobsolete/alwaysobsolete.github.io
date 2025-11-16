f1073fd70be9bf85cbe76cc978aac8faf664dfc4
alwaysobsolete <alwaysobsolete@users.noreply.github.com>
2025-11-15 19:40:01 -0500

    ADD - GoogleAnalytics wrapper component

---
+++ src/components/google/GoogleAnalytics.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import type { GAParams } from "node_modules/@next/third-parties/dist/types/google";
+import type { FC } from "react";
+import { GoogleAnalytics as NextGoogleAnalytics } from "@next/third-parties/google";
+
+import { useGPCContext } from "@/contexts/GPC";
+
+const GoogleAnalytics: FC<GAParams> = ({
+	gaId,
+	dataLayerName,
+	debugMode,
+	nonce,
+}) => {
+	/*
+	 * Context
+	 */
+	const { enabled: gpcEnabled } = useGPCContext();
+
+	/*
+	 * React element
+	 */
+	if (gpcEnabled) {
+		return;
+	} else {
+		return (
+			<NextGoogleAnalytics
+				gaId={gaId}
+				dataLayerName={dataLayerName}
+				debugMode={debugMode}
+				nonce={nonce}
+			/>
+		);
+	}
+};
+
+export default GoogleAnalytics;
